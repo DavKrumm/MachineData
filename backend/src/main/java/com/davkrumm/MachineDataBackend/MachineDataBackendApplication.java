5ce93acc001aa3ff3e7490879e0d0b24a972338b
DavKrumm <167351367+DavKrumm@users.noreply.github.com>
2024-09-03 12:23:00 +0200

    initial test commit

---
+++ backend/src/main/java/com/davkrumm/MachineDataBackend/MachineDataBackendApplication.java
@@ -0,0 +1,13 @@
+package com.davkrumm.MachineDataBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MachineDataBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MachineDataBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/davkrumm/MachineDataBackend/MachineDataBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davkrumm.MachineDataBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MachineDataBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
